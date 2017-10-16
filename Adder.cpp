af248119aa953b821b794b6029658496c6c3e354
AllenWang <allen5261@foxmail.com>
2017-10-16 17:12:48 +0800

    OO

--- Adder.cpp
+++ Adder.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-	string sub;
+	string sub,ppt,ssub="Lorem";
 	cin>>sub;
-	while(sub=="exit"){
-		if(sub=="chi") freopen("Chinese.html","a+",stdout);
-			else if(sub=="mat") freopen("Maths.html","a+",stdout);
-			else if(sub=="eng") freopen("English.html","a+",stdout);
-			else if(sub=="che") freopen("Chemistry.html","a+",stdout);
-			else if(sub=="his") freopen("History.html","a+",stdout);
+	do{
+//		if(sub=="chi") {freopen("Chinese.html","a+",stdout);ssub="语文"; }
+//			else if(sub=="mat") {freopen("Maths.html","a+",stdout);ssub="数学"; }
+//			else if(sub=="eng") {freopen("English.html","a+",stdout);ssub="英语"; }
+//			else if(sub=="che") {freopen("Chemistry.html","a+",stdout);ssub="化学"; }
+//			else if(sub=="his") {freopen("History.html","a+",stdout);ssub="历史"; }
+	
+		cin>>ppt;
+		
+		cout<<"\t\t<tr>\n";
+		cout<<"\t\t<td><font color=\"#FFFFFF\" face=\"source-han-serif-sc\"><span lang=\"zh-cn\">"<<ppt<<"</span></font></td>\n";
+		cout<<"\t\t<td align=\"right\"><font color=\"#FFFFFF\" face=\"Courier New\">7.98MB</font></td>"
+		
 		cin>>sub;
-	}
+	}while(sub!="exit");
+	return 0; 
 } 
---
+++ oo.cpp
@@ -0,0 +1,9 @@
+\t\t<tr>
+\t\t<td><font color=\"#FFFFFF\" face=\"source-han-serif-sc\"><span lang=\"zh-cn\">20. 上课 人类迈入电气时代.ppt</span></font></td>
+\t\t<td align=\"right\"><font color=\"#FFFFFF\" face=\"Courier New\">7.98MB</font></td>
+
+\t\t<td align=\"center\" width=\"10\"><font face=\"source-han-serif-sc\" color=\"#FFFFFF\">
+\t\t<a href=\"./files/历史/20. 上课 人类迈入电气时代.ppt\" target=\"_blank\">
+\t\t<input type=\"button\" value=\"下载\" style=\"float: right\"></a></font></td>
+
+\t\t</tr>
