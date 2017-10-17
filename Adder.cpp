e641fdb947eea7130dd4cd10da0f565d339aa076
AllenWang <allen5261@foxmail.com>
2017-10-17 17:08:36 +0800

    New

--- Adder.cpp
+++ Adder.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include <sys/types.h>  
+#include <sys/stat.h>
 using namespace std;
+long getFileSize(char* strFileName)
+{
+    struct _stat info;
+    _stat(strFileName, &info);
+    long size = info.st_size;
+    return size;
+} 
 int main(){
 	string sub,ppt,ssub="Lorem";
+	char* path;
+	long s;
 	cin>>sub;
 	do{
 //		if(sub=="chi") {freopen("Chinese.html","a+",stdout);ssub="ÓïÎÄ"; }
@@ -12,9 +23,17 @@ int main(){
 	
 		cin>>ppt;
 		
+		
+		
+		cout<<s<<endl;
+		
 		cout<<"\t\t<tr>\n";
 		cout<<"\t\t<td><font color=\"#FFFFFF\" face=\"source-han-serif-sc\"><span lang=\"zh-cn\">"<<ppt<<"</span></font></td>\n";
-		cout<<"\t\t<td align=\"right\"><font color=\"#FFFFFF\" face=\"Courier New\">7.98MB</font></td>"
+		cout<<"\t\t<td align=\"right\"><font color=\"#FFFFFF\" face=\"Courier New\">"<<s<<"</font></td>";
+		cout<<"\t\t<td align=\"center\" width=\"10\"><font face=\"source-han-serif-sc\" color=\"#FFFFFF\">\n";
+		cout<<"\t\t<a href=\""<<path<<"\" target=\"_blank\">\n";
+		cout<<"\t\t<input type=\"button\" value=\"ÏÂÔØ\" style=\"float: right\"></a></font></td>\n";
+		cout<<"\t\t</tr>\n\t\t\n";
 		
 		cin>>sub;
 	}while(sub!="exit");
